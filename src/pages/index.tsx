45cb47db4b4a956b73055229a4fe302a5d0a5920
cleisonmp <cleison.mp@gmail.com>
2022-09-12 13:30:08 -0300

    init: basic setup :tada:

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Github - Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/posts/[number].tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Post: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>PostName</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </>
+  );
+};
+
+export default Post;
